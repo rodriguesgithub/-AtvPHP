2f40e55155d8ab6020debca0141d5080ee6a76d3
Acer 13 <super.jonathan.de.lima@gmail.com>
2019-06-14 00:22:47 -0300

    verifica se as senhas são iguais

--- processa.php
+++ processa.php
@@ -4,10 +4,28 @@ include_once("conexao.php");
 
 $nome = $_POST['nome'];
 $email = $_POST['email'];
-$senha = $_POST['pswd1'];
+$senha1 = $_POST['pswd1'];
+$senha2 = $_POST['pswd2'];
 
-$query = "insert into info (nome,email,senha) values ('$nome','$email','$senha')";
-$salvar = mysqli_query($conexao, $query);
+if ($senha1 == $senha2) {
+	$query = "insert into info (nome,email,senha) values ('$nome','$email','$senha1')";
+	$salvar = mysqli_query($conexao, $query);
+	echo "<script>javascript:alert('Usuário Cadastrado')</script>";
+	echo "<script>javascript:window.location='index.php'</script>";
+
+	echo "<script>window.setTimeout(function() {header('Location: index.php');}, 3000);</script>";
+	 
+}else{
+	
+    echo "<script>javascript:alert('As senhas estão diferentes')</script>";
+	echo "<script>javascript:window.location='cadastrar.php'</script>";
+
+	$var = "<script>javascript:history.back(-2)</script>";
+    echo $var;
+}
+
+
+
 
 mysqli_close($conexao);
 
